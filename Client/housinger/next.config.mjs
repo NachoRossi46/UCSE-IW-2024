3466380ef63afb13141d4ad126b3ad005e3b833e
Ignacio <nachorossi121@gmail.com>
2024-08-19 23:41:14 -0300

    config

--- Client/housinger/next.config.mjs
+++ Client/housinger/next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['tu-dominio-de-imagenes.com'],
+    },
+    env: {
+      customKey: process.env.CUSTOM_KEY,
+      // Añade aquí otras variables de entorno que necesites
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
+    // Añade aquí otras configuraciones específicas de tu proyecto
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
